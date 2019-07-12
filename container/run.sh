2af7651788521ac9d698ba0e0e7269726e732206
Justin Tamblyn <just1689@gmail.com>
2019-07-12 09:23:18 +0200

    Faster startup

--- container/run.sh
+++ container/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-java -Xms2G -Xmx2G -XX:+UseConcMarkSweepGC -jar spigot-1.14.3.jar
+java -Xms2G -Xmx2G -XX:+UseConcMarkSweepGC -DIReallyKnowWhatIAmDoingISwear -jar spigot-1.14.3.jar
 
 sleep forever
